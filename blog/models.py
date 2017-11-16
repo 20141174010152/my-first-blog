1db2af8dbc5f45e6e5fea0f77085b9299eb5c0ce
Bruno Wagner <bruno.wagner.b.r@gmail.com>
2017-11-16 01:42:43 -0200

    Testando nova função

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,21 @@ class Post(models.Model):
 	      default=timezone.now)
 	published_date = models.DateTimeField(
 	      blank=True, null=True)
+	FRESHMAN = 'FR'
+	SOPHOMORE = 'SO'
+	JUNIOR = 'JR'
+	SENIOR = 'SR'
+	YEAR_IN_SCHOOL_CHOICES = (
+	  (FRESHMAN, 'Freshman'),
+	  (SOPHOMORE, 'Sophomore'),
+	  (JUNIOR, 'Junior'),
+	  (SENIOR, 'Senior'),
+	)
+	year_in_school = models.CharField(
+	  max_length=2,
+	  choices=YEAR_IN_SCHOOL_CHOICES,
+	  default=FRESHMAN,
+	)
 
 	def publish(self):
 	  self.published_date = timezone.now()
